e3d583f71be8fe6a11a1b6df8287f761e4fb84d4
demidraksin <greatraksin@icloud.com>
2022-10-07 18:27:37 +0300

    Initial commit

---
+++ 03_lesson_2209/00_input_типы_данных.py
@@ -0,0 +1,6 @@
+value = float(input('Введите значение: '))
+print(value / 23)
+
+value1 = eval(input('Введите математическое выражение: '))
+print(value1)
+
---
+++ 04_lesson_2909/02_is_и_in.py
@@ -0,0 +1,8 @@
+x = 5
+y = x
+
+print(x is y)
+
+animal = 'elephant'
+print('eleph' in animal)  # True - буквы eleph Нахоодятся внутри слова elephant
+print('ehtp' in animal)  # False - буквы ehtp НЕ нахоодятся внутри слова elephant в таком порядке
