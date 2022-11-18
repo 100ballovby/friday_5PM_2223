9dc1fe9feb2c9299af690e565396847db6c4478d
demidraksin <greatraksin@icloud.com>
2022-11-18 18:09:48 +0300

    Adding dictionaries

---
+++ 12_словари/data.py
@@ -0,0 +1,49 @@
+d1 = {
+    "name": "Николаева Мария",
+    "birthday": "14/07/1998",
+    "height": 180,
+    "weight": 66.9,
+    "car": True,
+    "languages": [
+      "C#",
+      "C++",
+      "C"
+    ]
+  }
+
+d2 = [
+  {
+    "name": "Иванов Иван",
+    "birthday": "01/12/2000",
+    "height": 170,
+    "weight": 70.5,
+    "car": True,
+    "languages": [
+      "C++",
+      "Python"
+    ]
+  },
+  {
+    "name": "Сергеев Сергей",
+    "birthday": "01/06/2001",
+    "height": 180,
+    "weight": 110.4,
+    "car": False,
+    "languages": [
+      "Pascal",
+      "Delphi"
+    ]
+  },
+  {
+    "name": "Николаева Мария",
+    "birthday": "14/07/1998",
+    "height": 180,
+    "weight": 66.9,
+    "car": True,
+    "languages": [
+      "C#",
+      "C++",
+      "C"
+    ]
+  }
+]
