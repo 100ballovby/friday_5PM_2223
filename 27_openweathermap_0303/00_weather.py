c9945661fa1d33ce1fdd5c27c6ced35c5aa24f91
greatraksin <greatraksin@icloud.com>
2023-03-03 18:19:00 +0300

    Adding get_weather function

--- 27_openweathermap_0303/00_weather.py
+++ 27_openweathermap_0303/00_weather.py
@@ -2,13 +2,29 @@ import requests as r
 import os
 
 
-def get_weather():
+def get_weather(city):
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
-        'appid': '',
+        'appid': os.environ.get('API_KEY'),  # получаем переменную среды
+        'units': 'metric',
+        'lang': 'ru',
+        'q': city,
     }
     res = r.get(url, params=params)
     return res.json()
 
 
-print(get_weather())
+def show_weather(json_response):
+    print(f'Город: {json_response["name"]}')
+    print(f'Температура: {json_response["main"]["temp"]}')
+    print(f'Ощущается как: {json_response["main"]["feels_like"]}')
+    print(f'На улице {json_response["weather"][0]["description"]}')
+    print(f'Максимальная температура за день: {json_response["main"]["temp_max"]}')
+    print(f'Минимальная температура за день: {json_response["main"]["temp_min"]}')
+    print('\n\n')
+
+cities = ['москва', 'Санкт-Петербург', 'Minsk', 'Paris', 'Warsaw', 'New York', 'Vilnius']
+
+for city in cities:
+    ans = get_weather(city)
+    show_weather(ans)
