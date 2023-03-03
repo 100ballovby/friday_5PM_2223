8f0d7d580bc182efd948c3764eea8464e6991e9d
greatraksin <greatraksin@icloud.com>
2023-03-03 17:23:08 +0300

    Adding get_weather function

--- 27_openweathermap_0303/00_weather.py
+++ 27_openweathermap_0303/00_weather.py
@@ -1,9 +1,14 @@
 import requests as r
+import os
 
 
 def get_weather():
-    url = "https://api.openweathermap.org/v2/weather"
-    params = {}
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'appid': '',
+    }
     res = r.get(url, params=params)
     return res.json()
 
+
+print(get_weather())
