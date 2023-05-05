5a182fd8f68f1e492e980ae973a23b1e7ea7b54d
greatraksin <greatraksin@icloud.com>
2023-05-05 17:04:58 +0300

    Adding first game code

--- 30_pygame_2104/pygame_boilerplate.py
+++ 30_pygame_2104/pygame_boilerplate.py
@@ -1,8 +1,11 @@
 import sys
 import pygame as pg
 
+WIN_WIDTH = 1280
+WIN_HEIGHT = 720
+
 pg.init()  # инициализируем pygame
-pg.display.set_mode((1280, 720))  # создаем экран игры разрешением 1280х720px
+pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))  # создаем экран игры разрешением 1280х720px
 
 while True:  # цикл игры
     for event in pg.event.get():  # обработчик событий pygame
