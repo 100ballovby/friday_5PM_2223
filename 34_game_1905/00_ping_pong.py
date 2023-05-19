661e0ca5ff66cbbe0ceaee927c7a793785033df3
greatraksin <greatraksin@icloud.com>
2023-05-19 18:10:15 +0300

    Adding player animation

--- 34_game_1905/00_ping_pong.py
+++ 34_game_1905/00_ping_pong.py
@@ -7,30 +7,39 @@ def ball_move(ball):
     ball.x += speed_x
     ball.y += speed_y
 
-    if ball.top <= 0 or ball.bottom >= WIN_HEIGHT:  # если мяч ударился об верхнюю или нижнюю границу экрана
+    if ball.top <= 0 or ball.bottom >= H:  # если мяч ударился об верхнюю или нижнюю границу экрана
         speed_y *= -1  # развернуть его в обратную сторону
-    elif ball.left <= 0 or ball.right >= WIN_WIDTH:
+    elif ball.left <= 0 or ball.right >= W:
         speed_x *= -1
     elif ball.colliderect(player) or ball.colliderect(opponent):
         speed_x *= -1
+        
+        
+def player_motion(speed, pl):
+    pl.y += speed 
+    
+    if pl.top <= 0:  # если мы ушли за верхнюю границу экрана
+        pl.top = 0  # останавливаем платформу там 
+    elif pl.bottom >= H:
+        pl.bottom = H
 
 
-WIN_WIDTH = 1280
-WIN_HEIGHT = 720
+W = 1280
+H = 720
 
 GRAY = (230, 230, 230)
 WHITE = (255, 255, 255)
 GREEN = (83, 247, 74)
 
 pg.init()  # инициализируем pygame
-screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))  # создаем экран игры разрешением 1280х720px
+screen = pg.display.set_mode((W, H))  # создаем экран игры разрешением 1280х720px
 clock = pg.time.Clock()
 FPS = 60
 
 # игровые сущности
-player = pg.Rect(WIN_WIDTH - 20, WIN_HEIGHT // 2, 10, 150)
-opponent = pg.Rect(10,  WIN_HEIGHT // 2, 10, 150)
-ball = pg.Rect(WIN_WIDTH // 2 - 15, WIN_HEIGHT // 2 - 15, 30, 30)
+player = pg.Rect(W - 20, H // 2, 10, 150)
+opponent = pg.Rect(10,  H // 2, 10, 150)
+ball = pg.Rect(W // 2 - 15, H // 2 - 15, 30, 30)
 
 # переменные для игры
 p_speed = 0
@@ -49,11 +58,21 @@ while not finished:  # цикл игры
 
     # отображение игровых объектов
     screen.fill(GRAY)
-    pg.draw.aaline(screen, WHITE, [WIN_WIDTH // 2, 0], [WIN_WIDTH // 2, WIN_HEIGHT])
+    pg.draw.aaline(screen, WHITE, [W // 2, 0], [W // 2, H])
     pg.draw.rect(screen, GREEN, player)
     pg.draw.rect(screen, GREEN, opponent)
     pg.draw.ellipse(screen, GREEN, ball)
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_UP]:
+        p_speed -= 5
+    elif keys[pg.K_DOWN]:
+        p_speed += 5
+    else:
+        p_speed = 0
+
     pg.display.update()
 
     # логика игры
     ball_move(ball)
+    player_motion(p_speed, player)
